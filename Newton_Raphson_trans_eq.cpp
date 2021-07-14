44c6a5c29769a820940768bc15b89565b0c267e3
Paul Gavriloaea <paulgavriloaea@gmail.com>
2021-07-14 12:09:12 +0200

    Added for loop functionality, from 0 to Tc, calculate m_e(T)

--- Newton_Raphson_trans_eq.cpp
+++ Newton_Raphson_trans_eq.cpp
@@ -13,39 +13,61 @@ double g(double x, double T, double Tc, double eps)
 int main()
 {
 double a,b,d,n;
-double T = 600.0;
-double Tc=631.0;
+int T = 600.0;
+int Tc=631.0;
 double eps = 1.0;
-int i;
-cout<<" Given Trancedental equation: eps*T/Tc)*x - 1.0/(tanh(x)) + 1.0/x "<<endl;
-cout<<" Enter the initial guess of the root "<<endl;
-cin>>a;
-cout<<" Enter the number of Iterations "<<endl;
-cin>>i;
-if(f(a,T,Tc,eps) != 0.0)
+int size = Tc+1; //including T=0
+int T_arr[size];
+double me_arr[size];
+double TOL = 1e-7;
+
+int N_iter = 50;
+
+
+a = 5.0; //initial Guess
+
+T_arr[0] = 0; //Deal with the 0K case
+me_arr[T] = 1.0;
+
+T_arr[Tc+1]=Tc; //Deal with the T=Tc case
+me_arr[Tc+1]=0.0;
+
+
+for(T = 1;T<Tc; T++ )//Loop temperatures between 1K and Tc-1
 {
- for(n=1;n<=i;n++)
- {
-  b=a-(f(a,T,Tc,eps)/g(a,T,Tc,eps));
-  if(f(b,T,Tc,eps)==0)
-  {
-   cout<<" i = "<<n<<" Root of the given equation is "<<b<<endl;
-  }
-  else
-  {
-   d=fabs((b-a)/b);
-   cout<<" i = "<<n<<" a = "<<a<<" b = "<<b<<" |E| = "<<d*100<<" % "<<endl;
-   a=b;
-  }
- }
- cout<<" Root of the given equation is "<<b<<endl;
+    if(f(a,T,Tc,eps) != 0.0) //If the initial point is not solution, then search
+    {
+        for(int i=1;i<=N_iter;i++)
+        {
+            b=a-(f(a,T,Tc,eps)/g(a,T,Tc,eps));
+            if(fabs(f(b,T,Tc,eps))<TOL) //If b is solution then stop 
+            {
+                T_arr[T] = T;
+                me_arr[T] = (b*eps)/(3*Tc);
+                break;
+            }
+            else
+            {
+                //d=fabs((b-a)/b); Error
+                //cout<<" i = "<<n<<" a = "<<a<<" b = "<<b<<" |E| = "<<d*100<<" % "<<endl;
+                a=b; //the new initial point is swapped with b
+            }
+        }
+        //if for loop didn't break, consider the solution to be the last point.
+        T_arr[T] = T;
+        me_arr[T] = (b*eps)/(3*Tc);
+
+    }
+    else{
+        T_arr[T] = T;
+        me_arr[T] = (a*eps)/(3*Tc);
+    }
 }
-else
-{
- cout<<" Root of the given equation is "<<a<<endl;
-}
+
+
 return 0;
 }
+
 
 /*Result
  Given Trancedental equation is sin(x)-cos(x)
